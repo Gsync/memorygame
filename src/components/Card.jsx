81ff7fe2952de8005da55ef6eb9f11ceb56f3a46
Khuram Niaz <knshaikh@gmail.com>
2018-02-22 15:20:35 -0700

    Card component and styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import './Card.css';
 
-class Card extends Component {
-    render() {
-        return (
-            <div>Card comp</div>
-        );
+const Card = (props) => {
+    let style = {};
+    if (props.showing) {
+        style.backgroundColor = props.backgroundColor;
     }
-}
+    return (
+        <div onClick={props.on} className="card" style={style}>Card</div>
+    );
+};
+
+Card.propTypes = {
+    showing: PropTypes.bool.isRequired,
+    backgroundColor: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired
+};
 
 export default Card;
